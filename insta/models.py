00873cffdccd37bc71a0487f1b3c1e6d834d8e42
Bernice2001 <wangechibanim@gmail.com>
2020-10-27 16:00:40 +0300

    update models.py

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.TextField(default="Anonymous")
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    location = models.CharField(max_length=60, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    def save_profile(self):
+        self.user
+    def delete_profile(self):
+        self.delete()
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(owner=id)
+        return profile
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()class Profile(models.Model):
