c8f3635aa4e1a6383be624ac5034a00b0098f409
Bernice2001 <wangechibanim@gmail.com>
2020-10-28 11:43:10 +0300

    url update

--- insta/urls.py
+++ insta/urls.py
@@ -3,10 +3,15 @@ from . import views
 from django.urls import path
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     # url('^$', views.home, name='homepage'),
     path('signup/', views.signup, name='signup'),
+    path('profile/$', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('post/<id>', views.post_comment, name='comment'),
     url('accounts/', include('django.contrib.auth.urls')),
     url(r'Home/$', views.Home, name='Home'),
     url(r'^profile/(\d+)$', views.profile, name='profile'),
+    url(r'^editprofile/',views.profile,name ='profile'),
 ]
